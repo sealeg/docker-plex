ec0fe91c051fd688762d9b6f50b37b2a291e54d7
sealeg <sealeg@lemonman.org.uk>
2016-08-13 02:10:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# plex
+
+Plex Media Server docker image.
+
+## Usage
+
+To run on the default port 32400:
+
+```
+docker run -d -v <data_dir>:/data -v <media_dir>:/media_library -p 32400:32400 sealeg/plex
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    data_dir=/data
+    if [ ! -d "$data_dir" ]; then
+        printf 'Config dir (%s) is not present.\n' $data_dir
+
+        exit 1
+    fi
+
+    media_dir=/media_library
+    if [ ! -d "$media_dir" ]; then
+        printf 'Media dir (%s) is not present.\n' $media_dir
+
+        exit 1
+    fi
+
+    printf 'Setting permissions...'
+    chown -R plex.plex /data/*
+    printf ' done\n'
+
+    LD_LIBRARY_PATH=/usr/lib/plexmediaserver \
+    exec gosu plex /usr/lib/plexmediaserver/Plex\ Media\ Server
+else
+    exec "$@"
+fi
+
